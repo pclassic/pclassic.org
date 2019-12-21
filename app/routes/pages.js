b5edb4332b5e7fa98e77098ae59cedcb8887b041
Brian Chen <bychen9@gmail.com>
2019-12-21 12:02:47 -0800

    update fall 2019 route

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,5 +1,5 @@
 module.exports = [
   'rules', 'problems', 'faq', 'mail', 'photos', 'history', 'organizers',
   '2012', '2013s', '2013f', '2014s', '2014f', '2015s', '2015f', '2016s', '2016f', '2017s',
-  '2017f', '2018s', '2018f', '2019s'
+  '2017f', '2018s', '2018f', '2019s', '2019f'
 ];
